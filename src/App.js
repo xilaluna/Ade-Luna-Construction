571fe7d2558cd51d2eca0788d117f0450bf51ceb
xiluna <xilaluna2@gmail.com>
2022-03-08 17:28:05 -0800

    [add] navbar & init home

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react"
+import Navbar from "./components/Navbar/Navbar"
 
 function App() {
   return (
     <React.Fragment>
+      <Navbar />
       <h1>Hello</h1>
     </React.Fragment>
   )
