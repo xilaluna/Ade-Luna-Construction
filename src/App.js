5f97bcdf9beb6e88cdb77b4e16deb24af7e31135
xiluna <xilaluna2@gmail.com>
2022-03-09 14:10:14 -0800

    [add] init pages & react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from "react"
+
 import Navbar from "./components/Navbar/Navbar"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Services from "./pages/Services"
+import Projects from "./pages/Projects"
+import Contact from "./pages/Contact"
+
+import { Routes, Route } from "react-router-dom"
 
 function App() {
   return (
     <React.Fragment>
       <Navbar />
-      <h1>Hello</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="services" element={<Services />} />
+        <Route path="projects" element={<Projects />} />
+        <Route path="contact" element={<Contact />} />
+      </Routes>
     </React.Fragment>
   )
 }
--- src/index.js
+++ src/index.js
@@ -2,15 +2,21 @@ import React from "react"
 import ReactDOM from "react-dom"
 import App from "./App"
 
+// mui
 import CssBaseline from "@mui/material/CssBaseline"
 import { ThemeProvider } from "@mui/material/styles"
 import theme from "./styles/theme"
+
+// react router
+import { BrowserRouter } from "react-router-dom"
 
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
